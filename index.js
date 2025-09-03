58e446fee4d9e95cc7585934dbcef1fcf46c4f7f
faith-kerry <kerubofaith944@gmail.com>
2025-09-04 01:18:53 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 20; // Changed from 74 to 20
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+const expect = require('chai').expect;
+
+describe("Height", () => {
+    it("is greater than 0 and less than 40", () => {
+      expect(height).to.be.above(0);
+      expect(height).to.be.below(40);
+    });
+});
